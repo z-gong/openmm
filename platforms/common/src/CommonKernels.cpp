56acacab35278bf345c7fb952ef9a8c73e764d2e
Peter Eastman <peastman@stanford.edu>
2024-12-16 15:42:57 -0800

    Prevent extra force evaluations with NoseHooverIntegrator (#4753)
    (cherry picked from commit 87810125957f807e7b1688091e5e4546dbe1d3b6)

--- platforms/common/src/CommonKernels.cpp
+++ platforms/common/src/CommonKernels.cpp
@@ -5640,7 +5640,7 @@ void CommonIntegrateNoseHooverStepKernel::initialize(const System& system, const
         energyBuffer.initialize<mm_float2>(cc, energyBufferSize, "energyBuffer");
 }
 
-void CommonIntegrateNoseHooverStepKernel::execute(ContextImpl& context, const NoseHooverIntegrator& integrator, bool &forcesAreValid) {
+void CommonIntegrateNoseHooverStepKernel::execute(ContextImpl& context, const NoseHooverIntegrator& integrator) {
     ContextSelector selector(cc);
     IntegrationUtilities& integration = cc.getIntegrationUtilities();
     int paddedNumAtoms = cc.getPaddedNumAtoms();
@@ -5649,7 +5649,8 @@ void CommonIntegrateNoseHooverStepKernel::execute(ContextImpl& context, const No
 
     // If the atom reordering has occured, the forces from the previous step are permuted and thus invalid.
     // They need to be either sorted or recomputed; here we choose the latter.
-    if (!forcesAreValid || cc.getAtomsWereReordered()) context.calcForcesAndEnergy(true, false, integrator.getIntegrationForceGroups());
+    if (cc.getAtomsWereReordered())
+        context.calcForcesAndEnergy(true, false, integrator.getIntegrationForceGroups());
 
     const auto& atomList = integrator.getAllThermostatedIndividualParticles();
     const auto& pairList = integrator.getAllThermostatedPairs();
